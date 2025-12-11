3168658a8ed27c2bb68e6bf8ce37a77a63eee1e1
saikumarpinagani12-wq <saikumarpinagani12@gmail.com>
2025-12-11 12:00:01 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,22 @@
+/* 
+public class loops {
+    public static void main(String[] args) {
+        // Print numbers from 1 to 10 using a for loop
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("suswaran");
+        }
+    }
+    
+}
+*/
+
+public class loops {
+    public static void main(String[] args) {
+        for(int i=0;i<=100;i++){
+
+            if(i%2==0)
+                System.out.print(i+",");
+            }
+        }
+}
+        
