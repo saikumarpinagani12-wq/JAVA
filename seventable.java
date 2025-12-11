5f6c31cf15c71b2988d9100c4a1dd98c653302d3
saikumarpinagani12-wq <saikumarpinagani12@gmail.com>
2025-12-11 12:12:17 +0530

    Add files via upload

---
+++ seventable.java
@@ -0,0 +1,13 @@
+public class seventable {
+    
+    public static void main(String[] args) {
+        int num=7;
+        for(int i=1;i<=5;i++){
+            System.out.println(num+"*"+i+"="+mul(num,i));
+        }
+    }
+    public static int mul(int a,int b){
+        return a*b;
+    }
+    
+}
