a16a8a25e77d2396010f44ccd860582dd5fe0dce
saikumarpinagani12-wq <saikumarpinagani12@gmail.com>
2025-12-12 12:07:46 +0530

    Add files via upload

---
+++ PalindromeCheck.java
@@ -0,0 +1,20 @@
+public class PalindromeCheck {
+    public static boolean isPalindrome(String str) {
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
+        }
+
+        return str.equals(reversed);
+    }
+    public static void main(String[] args) {
+        String str = "madam";
+
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a Palindrome");
+        } else {
+            System.out.println(str + " is NOT a Palindrome");
+        }
+    }
+}
