83439e8ec6dab3f5afb4c2e43797be2656e889ad
saikumarpinagani12-wq <saikumarpinagani12@gmail.com>
2025-12-12 09:35:17 +0530

    Add files via upload

---
+++ ArithmeticErrorexception.java
@@ -0,0 +1,14 @@
+public class ArithmeticErrorexception {
+    public static void main(String[] args) {
+        try {
+            int numerator = 50;
+            int denominator = 0; // This will cause ArithmeticException
+            int divisionResult = numerator / denominator;
+
+            System.out.println("Division Result: " + divisionResult);
+        } catch (ArithmeticException e) {
+            System.out.println("Caught ArithmeticException: " + e.getMessage());
+        }
+    }
+    
+}
